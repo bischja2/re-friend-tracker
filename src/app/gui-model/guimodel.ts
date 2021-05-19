41dc4211e34003cc5f62e581e19b8156d0022ce8
bischja2 <bischja2@students.zhaw.ch>
2021-05-19 14:24:22 +0000

    v4

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -66,14 +66,6 @@ export class GuiModel {
                     "url": "/friend",
                     "formFieldList": [
                         {
-                            "id": "group",
-                            "type": "autocomplete",
-                            "name": "Group",
-                            "url": "/group",
-                            "form": "GroupForm",
-                            "width": 2
-                        },
-                        {
                             "id": "familyName",
                             "type": "text",
                             "name": "FamilyName",
@@ -93,6 +85,14 @@ export class GuiModel {
                             "name": "Nickname",
                             "width": 2,
                             "required": true
+                        },
+                        {
+                            "id": "group",
+                            "type": "autocomplete",
+                            "name": "Group",
+                            "url": "/group",
+                            "form": "GroupForm",
+                            "width": 2
                         },
                         {
                             "id": "location",
